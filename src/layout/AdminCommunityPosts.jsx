9b5eb1a2240620817f95fd769754f3e60b798ef8
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-12-11 14:17:57 +0900

    feat: 게시글 상세보기 기능 추가

--- src/layout/AdminCommunityPosts.jsx
+++ src/layout/AdminCommunityPosts.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useSearchParams} from "react-router-dom";
 import {usersApi, postsApi, commentsApi} from "../api/api.js";
 import AdminSidebar from "./AdminSidebar.jsx";
 import "../css/AdminCommunityPosts.css";
@@ -17,6 +17,8 @@ function AdminCommunityPosts() {
     const [comments, setComments] = useState([]); // 댓글 데이터
     const [loadingComments, setLoadingComments] = useState(false); // 댓글 로딩 상태
     const [commentError, setCommentError] = useState(null); // 댓글 조회 에러
+    const [postDetails, setPostDetails] = useState(null);
+
 
     useEffect(() => {
         if (!userId) {
@@ -26,6 +28,7 @@ function AdminCommunityPosts() {
 
         fetchUserTokenAndDetails(userId);
     }, [userId]);
+
 
     const fetchUserTokenAndDetails = async (userId) => {
         setLoading(true);
@@ -38,7 +41,7 @@ function AdminCommunityPosts() {
             }
 
             const response = await usersApi.get(`/admin/users/${userId}`, {
-                headers: { Authorization: `Bearer ${adminAccessToken}` },
+                headers: {Authorization: `Bearer ${adminAccessToken}`},
             });
             setUserDetails(response.data);
             setApiToken(response.data.token);
@@ -60,9 +63,10 @@ function AdminCommunityPosts() {
         setError(null);
         try {
             const response = await postsApi.get(`/users/me`, {
-                headers: { Authorization: `Bearer ${token}` },
+                headers: {Authorization: `Bearer ${token}`},
             });
             setPosts(response.data);
+            console.log(response.data);
         } catch (err) {
             setError(err.response?.data?.message || "게시글 데이터를 가져오는 데 실패했습니다.");
         } finally {
@@ -70,12 +74,20 @@ function AdminCommunityPosts() {
         }
     };
 
+    const openPostDetailsModal = (post) => {
+        setPostDetails(post);
+    };
+
+    const closePostDetailsModal = () => {
+        setPostDetails(null);
+    };
+
+
     const fetchComments = async (postId) => {
         setLoadingComments(true);
         setCommentError(null);
         try {
-            const response = await commentsApi.get(`/${postId}/comments`, {
-            });
+            const response = await commentsApi.get(`/${postId}/comments`, {});
             setComments(response.data);
         } catch (err) {
             setCommentError(err.response?.data?.message || "댓글 데이터를 가져오는 데 실패했습니다.");
@@ -98,7 +110,7 @@ function AdminCommunityPosts() {
             }
 
             const tokenResponse = await usersApi.get(`/admin/users/${userId}`, {
-                headers: { Authorization: `Bearer ${adminAccessToken}` },
+                headers: {Authorization: `Bearer ${adminAccessToken}`},
             });
 
             const userToken = tokenResponse.data.token; // 받아온 사용자 토큰
@@ -124,14 +136,13 @@ function AdminCommunityPosts() {
     };
 
 
-
     const deletePost = async (postId) => {
         const confirmDelete = window.confirm("이 게시글을 삭제하시겠습니까?");
         if (!confirmDelete) return;
 
         try {
             await postsApi.delete(`/${postId}`, {
-                headers: { Authorization: `Bearer ${apiToken}` },
+                headers: {Authorization: `Bearer ${apiToken}`},
             });
 
             alert("게시글이 성공적으로 삭제되었습니다.");
@@ -151,124 +162,141 @@ function AdminCommunityPosts() {
         setSelectedPost(null);
     };
 
-    return (
-        <div className="admin-layout">
-            <AdminSidebar />
-            <div className="admin-content">
-                {error && <p className="error-message">{error}</p>}
+    return (<div className="admin-layout">
+        <AdminSidebar/>
+        <div className="admin-content">
+            {error && <p className="error-message">{error}</p>}
 
-                {loading && (
-                    <div className="loading-container">
-                        <div className="spinner"></div>
-                        <p>데이터를 불러오는 중...</p>
-                    </div>
-                )}
+            {loading && (<div className="loading-container">
+                <div className="spinner"></div>
+                <p>데이터를 불러오는 중...</p>
+            </div>)}
 
-                {!loading && userDetails && (
-                    <div className="user-details-container">
-                        <h2>게시글 관리</h2>
-                    </div>
-                )}
+            {!loading && userDetails && (<div className="user-details-container">
+                <h2>게시글 관리</h2>
+            </div>)}
 
-                {!loading && (
-                    <div className="table-container">
-                        {posts.length > 0 ? (
-                            <table className="custom-table">
-                                <thead>
-                                <tr>
-                                    <th>제목</th>
-                                    <th>내용</th>
-                                    <th>카테고리</th>
-                                    <th>작성일</th>
-                                    <th>좋아요</th>
-                                    <th>댓글 관리</th>
-                                    <th>삭제</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {posts.map((post) => (
-                                    <tr key={post.id}>
-                                        <td>{post.title}</td>
-                                        <td>
-                                            {post.content.length > 20
-                                                ? `${post.content.slice(0, 20)}...`
-                                                : post.content}
-                                        </td>
-                                        <td>{post.category}</td>
-                                        <td>{new Date(post.createdAt).toLocaleDateString()}</td>
-                                        <td>{post.likesCount}</td>
-                                        <td>
-                                            <button
-                                                className="comments-button"
-                                                onClick={() => openCommentsModal(post.id)}
-                                            >
-                                                댓글 보기
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                className="delete-button"
-                                                onClick={() => deletePost(post.id)}
-                                            >
-                                                삭제
-                                            </button>
-                                        </td>
-                                    </tr>
+            {!loading && (<div className="table-container">
+                {posts.length > 0 ? (<table className="custom-table">
+                    <thead>
+                    <tr>
+                        <th>제목</th>
+                        <th>내용</th>
+                        <th>카테고리</th>
+                        <th>작성일</th>
+                        <th>좋아요</th>
+                        <th>조회수</th>
+                        <th>댓글 관리</th>
+                        <th>삭제</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {posts.map((post) => (<tr
+                        key={post.id}
+                        onClick={() => openPostDetailsModal(post)}
+                        className="clickable-row"
+                    >
+                        <td>{post.title.length > 20 ? `${post.title.slice(0, 10)}...` : post.title}</td>
+                        <td>
+                            {post.content.length > 20 ? `${post.content.slice(0, 20)}...` : post.content}
+                        </td>
+                        <td>{post.category}</td>
+                        <td>{new Date(post.createdAt).toLocaleDateString()}</td>
+                        <td>{post.likesCount}</td>
+                        <td>{post.viewCount}</td>
+                        <td>
+                            <button
+                                className="comments-button"
+                                onClick={(e) => {
+                                    e.stopPropagation(); // 이벤트 버블링 방지
+                                    openCommentsModal(post.id); // 댓글 모달 열기
+                                }}
+                            >
+                                댓글 보기
+                            </button>
+                        </td>
+                        <td>
+                            <button
+                                className="delete-button"
+                                onClick={() => deletePost(post.id)}
+                            >
+                                삭제
+                            </button>
+                        </td>
+                    </tr>))}
+                    </tbody>
+                </table>) : (<p className="no-results">게시글이 없습니다.</p>)}
+            </div>)}
+
+            {selectedPost && (
+                <div className="modal-overlay" onClick={closeCommentsModal}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="close-button" onClick={closeCommentsModal}>
+                            &times;
+                        </button>
+                        <h2>댓글 관리</h2>
+                        {loadingComments ? (
+                            <div className="loading-container">
+                                <div className="spinner"></div>
+                                <p>댓글을 불러오는 중...</p>
+                            </div>
+                        ) : commentError ? (
+                            <p className="error-message">{commentError}</p>
+                        ) : comments.length > 0 ? (
+                            <ul className="comments-list">
+                                {comments.map((comment) => (
+                                    <li key={comment.id} className="comment-item">
+                                        <p>
+                                            <strong>작성자:</strong> {comment.userNickname}{" "}
+                                            <strong>유저 아이디:</strong> {comment.userId}
+                                        </p>
+                                        <p>
+                                            <strong>작성일:</strong>{" "}
+                                            {new Date(comment.createdAt).toLocaleString()}
+                                        </p>
+                                        <p>
+                                            <strong>댓글:</strong> {comment.content}
+                                        </p>
+                                        <button
+                                            className="delete-comment-button"
+                                            onClick={() => deleteComment(comment.id, comment.userId)}
+                                        >
+                                            삭제
+                                        </button>
+                                    </li>
                                 ))}
-                                </tbody>
-                            </table>
+                            </ul>
                         ) : (
-                            <p className="no-results">게시글이 없습니다.</p>
+                            <p className="no-results">댓글이 없습니다.</p>
                         )}
                     </div>
-                )}
+                </div>
+            )}
 
-                {selectedPost && (
-                    <div className="modal-overlay" onClick={closeCommentsModal}>
-                        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                            <button className="close-button" onClick={closeCommentsModal}>
-                                &times;
-                            </button>
-                            <h2>댓글 관리</h2>
-                            {loadingComments ? (
-                                <div className="loading-container">
-                                    <div className="spinner"></div>
-                                    <p>댓글을 불러오는 중...</p>
-                                </div>
-                            ) : commentError ? (
-                                <p className="error-message">{commentError}</p>
-                            ) : comments.length > 0 ? (
-                                <ul className="comments-list">
-                                    {comments.map((comment) => (
-                                        <li key={comment.id} className="comment-item">
-                                            <p className="comment-author">
-                                                <strong>작성자:</strong> {comment.userNickname} <strong>유저
-                                                아이디:</strong> {comment.userId}
-                                            </p>
-                                            <p className="comment-date">
-                                                <strong>작성일:</strong> {new Date(comment.createdAt).toLocaleString()}
-                                            </p>
-                                            <p className="comment-content"><strong>댓글: </strong> {comment.content}</p>
-                                            <p className="comment-content"><strong>좋아요: </strong>{comment.likeCount}</p>
-                                            <button
-                                                className="delete-comment-button"
-                                                onClick={() => deleteComment(comment.id, comment.userId)}
-                                            >
-                                                삭제
-                                            </button>
-                                        </li>
-                                    ))}
-                                </ul>
 
-                            ) : (
-                                <p className="no-results">댓글이 없습니다.</p>
-                            )}
-                        </div>
+            {/* 게시글 상세 모달 */}
+            {postDetails && (
+                <div className="modal-overlay" onClick={closePostDetailsModal}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="close-button" onClick={closePostDetailsModal}>
+                            &times;
+                        </button>
+                        <h2>게시글 상세 정보</h2>
+                        <p style={{ marginBottom: "20px" }}>
+                            <strong>제목:</strong> {postDetails.title}
+                        </p>
+                        <p style={{ marginBottom: "20px" }}>
+                            <strong>내용:</strong> {postDetails.content}
+                        </p>
+                        <p><strong>카테고리:</strong> {postDetails.category}</p>
+                        <p><strong>작성일:</strong> {new Date(postDetails.createdAt).toLocaleString()}</p>
+                        <p><strong>좋아요:</strong> {postDetails.likesCount}</p>
+                        <p><strong>조회수:</strong> {postDetails.viewCount}</p>
                     </div>
-                )}
-            </div>
+                </div>
+            )}
         </div>
-    );
+    </div>);
 }
 
 export default AdminCommunityPosts;
